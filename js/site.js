50ad65e8ac6fb5de225fa45c7e0a6518dc82a955
maroslaw <marek.brodziak@gmail.com>
2013-10-15 12:58:39 +0200

    #75 gh-pages switched to bootstrap

---
+++ js/site.js
@@ -0,0 +1,6 @@
+$('#show_tests').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $collapse = $this.closest('.collapse-group').find('.collapse');
+    $collapse.collapse('toggle');
+});
